d2e7c0928fb2981944f18242d446ac7ff692a255
Romero <103903074+MechaArms@users.noreply.github.com>
2022-06-28 14:38:32 -0300

    Create 8_kyu_Remove_String_Spaces.kt

---
+++ Kotlin/8_kyu_Remove_String_Spaces.kt
@@ -0,0 +1,9 @@
+/*
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+fun noSpace(x: String): String {
+    val filtered = x.filterNot { it.isWhitespace() }
+ 
+    return filtered
+}
