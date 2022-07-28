b82b28ed13acd4b869be65d6948267cf5140c422
Romero <103903074+MechaArms@users.noreply.github.com>
2022-07-28 08:44:52 -0300

    Create Small_enough-Beginner.kt

---
+++ Kotlin/Small_enough-Beginner.kt
@@ -0,0 +1,23 @@
+/*
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+*/
+
+
+//My solution
+//===========
+
+fun smallEnough(a : IntArray, limit : Int) : Boolean {
+    for(i in a){
+        if(i > limit){
+            return false
+        }
+    }
+    return true
+}
+
+//Best Solution
+//=============
+
+fun smallEnough(a: IntArray, limit: Int) = a.all { it <= limit }
