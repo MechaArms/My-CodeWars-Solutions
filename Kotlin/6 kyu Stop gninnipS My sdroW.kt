1f9d817e5446844f31b1c940f0af51e390244f7e
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-08 14:40:03 -0300

    Create 6 kyu Stop gninnipS My sdroW.kt

---
+++ Kotlin/6 kyu Stop gninnipS My sdroW.kt
@@ -0,0 +1,31 @@
+/*
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+*/
+
+//My Solution
+//===========
+
+package spinwords
+
+fun spinWords(sentence: String): String {
+    return sentence.split(" ")
+        .joinToString(" ") { word ->
+            if (word.length >= 5)
+                word.reversed()
+            else
+                word
+        }
+}
+
+//Best Solution
+//=============
+
+package spinwords
+
+fun spinWords(sentence: String) = sentence.split(" ").joinToString(" ") { if (it.length > 4) it.reversed() else it }
