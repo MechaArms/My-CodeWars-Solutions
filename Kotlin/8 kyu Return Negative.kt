da0fbc6b590c6858ff01432634a372b4c2ebe758
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-09 08:20:47 -0300

    Create 8 kyu Return Negative.kt

---
+++ Kotlin/8 kyu Return Negative.kt
@@ -0,0 +1,34 @@
+/*
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+Examples
+Kata().makeNegative(1)  // return -1
+Kata().makeNegative(-5) // return -5
+Kata().makeNegative(0)  // return 0
+Notes
+The number can be negative already, in which case no change is required.
+Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+*/
+
+//My Solution
+//===========
+
+class Kata {
+
+    fun makeNegative(x: Int): Int {
+        var nVal: Int = x
+        if (nVal < 0){
+            return nVal
+        }else{
+            return nVal.unaryMinus()
+        }
+    }
+}
+
+//Best Solution
+//=============
+
+class Kata {
+
+    fun makeNegative(x: Int) = if(x <= 0) x else -x
+}
