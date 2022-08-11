6bb673b1f801564164b9f2246fec51ee647ba28c
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-11 15:29:54 -0300

    Create 8 kyu Find the smallest integer in the array.kt

---
+++ Kotlin/8 kyu Find the smallest integer in the array.kt
@@ -0,0 +1,31 @@
+/*
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+*/
+
+//My Solution
+//===========
+
+package solution
+
+class SmallestIntegerFinder {
+
+  fun findSmallestInt(nums: List<Int>): Int {
+    return nums.sorted()[0]
+  }
+
+}
+
+//Best Solution
+//=============
+
+package solution
+
+class SmallestIntegerFinder {
+  fun findSmallestInt(nums: List<Int>) = nums.minOf { it }
+}
