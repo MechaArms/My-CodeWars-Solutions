13107ddefd75402500e5dac15120c4906e039546
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-17 14:45:34 -0300

    Convert a String to a Integer

--- Python/8 kyu Convert a String to a Number.py
+++ Python/8 kyu Convert a String to a Number.py
@@ -1,7 +1,7 @@
 '''
 Note: This kata is inspired by Convert a Number to a String!. Try that one too.
 
-Description
+Description 
 We need a function that can transform a string into a number. What ways of achieving this do you know?
 
 Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
