96c8a6c31ae60247390b52e233521cdde1a8ef01
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-17 14:46:31 -0300

    Convert a Boolean to a String

--- Python/8 kyu Convert a Boolean to a String.py
+++ Python/8 kyu Convert a Boolean to a String.py
@@ -1,7 +1,7 @@
 '''
 Implement a function which convert the given boolean value into its string representation.
 
-Note: Only valid inputs will be given.
+Note: Only valid inputs will be given. 
 '''
 
 def boolean_to_string(b):
