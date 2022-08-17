ffce4191cc6c9e8bd0cae8fbd1a2fd07acfaa9d7
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-17 14:43:48 -0300

    Count of positives and sum of negatives in a Array

--- Python/8 kyu Count of positives | sum of negatives.py
+++ Python/8 kyu Count of positives | sum of negatives.py
@@ -6,6 +6,7 @@ Return an array, where the first element is the count of positives numbers and t
 If the input is an empty array or is null, return an empty array.
 
 Example
+
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 '''
 
