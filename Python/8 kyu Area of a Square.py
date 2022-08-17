485098eda847ee801ec752d2ee11212d695068ee
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-17 14:49:59 -0300

    Find the square area with 1/4 of circle inside

--- Python/8 kyu Area of a Square.py
+++ Python/8 kyu Area of a Square.py
@@ -1,7 +1,7 @@
 '''
 Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
 
-Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc) 
 '''
 #graphic is a 1/4 of circle inside a square
 
