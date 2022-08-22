bbea8d55796e532f983f0f3cc460ff1100712fdf
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-22 10:07:07 -0300

    A list of nesting structures comparison

--- Python/4 kyu Nesting Structure Comparison.py
+++ Python/4 kyu Nesting Structure Comparison.py
@@ -1,6 +1,6 @@
 '''
 Complete the function/method (depending on the language) to return true/True when its argument is an array that has the same nesting structures and same corresponding length of nested arrays as the first array.
-
+ 
 For example:
 
 # should return True
