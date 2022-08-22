d1e093cf493f2c77b0b3c365a32829a1f12f80cc
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-22 10:05:18 -0300

    Strings strips filter based in makers

--- Python/4 kyu Strip Comments.py
+++ Python/4 kyu Strip Comments.py
@@ -1,6 +1,6 @@
 '''
 Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
-
+ 
 Example:
 
 Given an input string of:
