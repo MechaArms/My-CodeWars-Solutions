eb66f85e5d599949fcf166ffee06955369f62034
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-22 10:00:08 -0300

    Checks if two Arrays have the "same" elements

--- Python/6 kyu Are they the same.py
+++ Python/6 kyu Are they the same.py
@@ -2,6 +2,7 @@
 Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
 
 Examples
+
 Valid arrays
 a = [121, 144, 19, 161, 19, 144, 19, 11]  
 b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
@@ -9,6 +10,7 @@ comp(a, b) returns true because in b 121 is the square of 11, 14641 is the squar
 
 a = [121, 144, 19, 161, 19, 144, 19, 11] 
 b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
 Invalid arrays
 If, for example, we change the first number to something else, comp is not returning true anymore:
 
