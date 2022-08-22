4cfe7d07319fb42d4d2809fdda14756c69f0f33e
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-22 09:58:04 -0300

    Find the difference in the List and remove them

--- Python/6 kyu Array diff.py
+++ Python/6 kyu Array diff.py
@@ -1,4 +1,4 @@
-'''
+''' 
 Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 
 It should remove all values from list a, which are present in list b keeping their order.
