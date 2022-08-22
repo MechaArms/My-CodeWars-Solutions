07d4223f11f0ecdd41edbada07115dceab0742da
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-22 13:55:46 -0300

    Find the smallest integer in the Array

--- Kotlin/8 kyu Find the smallest integer in the array.kt
+++ Kotlin/8 kyu Find the smallest integer in the array.kt
@@ -5,7 +5,7 @@ For example:
 
 Given [34, 15, 88, 2] your solution will return 2
 Given [34, -345, -1, 100] your solution will return -345
-You can assume, for the purpose of this kata, that the supplied array will not be empty.
+You can assume, for the purpose of this kata, that the supplied array will not be empty. 
 */
 
 //My Solution
