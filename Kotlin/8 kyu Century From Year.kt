1a29397bd975f62e1a707e9894be433d7431fcff
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-22 13:59:50 -0300

    Find the century from a year

--- Kotlin/8 kyu Century From Year.kt
+++ Kotlin/8 kyu Century From Year.kt
@@ -3,7 +3,7 @@ Introduction
 The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
 
 Task
-Given a year, return the century it is in.
+Given a year, return the century it is in. 
 
 Examples
 1705 --> 18
