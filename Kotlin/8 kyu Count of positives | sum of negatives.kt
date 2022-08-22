f62be9bd2cad281cb796968785f7761f76ed8f5e
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-22 13:57:18 -0300

    Count of positives and sum of negatives in a Array

--- Kotlin/8 kyu Count of positives | sum of negatives.kt
+++ Kotlin/8 kyu Count of positives | sum of negatives.kt
@@ -1,7 +1,7 @@
 /*
 Given an array of integers.
 
-Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative. 
 
 If the input is an empty array or is null, return an empty array.
 
