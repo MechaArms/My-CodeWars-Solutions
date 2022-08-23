7e26c8ba1e15e96fd2a4c2d232755f2a5839bd29
Romero <103903074+MechaArms@users.noreply.github.com>
2022-08-23 07:48:08 -0300

    Substitute the words in a String for another one

--- Kotlin/7 kyu Complementary DNA.kt
+++ Kotlin/7 kyu Complementary DNA.kt
@@ -5,7 +5,7 @@ If you want to know more: http://en.wikipedia.org/wiki/DNA
 
 In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
-More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+More similar exercise are found here: http://rosalind.info/problems/list-view/ (source) 
 
 Example: (input --> output)
 
