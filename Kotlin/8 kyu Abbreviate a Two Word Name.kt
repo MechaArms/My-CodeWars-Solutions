9009a9d698d2725639dfd289e915e420bfbae5f7
Romero <103903074+MechaArms@users.noreply.github.com>
2022-10-18 09:48:55 -0300

    Abbraviate a Name

---
+++ Kotlin/8 kyu Abbreviate a Two Word Name.kt
@@ -0,0 +1,24 @@
+/*
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+*/
+
+/My Solution
+//===========
+
+fun abbrevName(name:String):String{
+    var nam = name.split(" ")
+    return (nam[0].get(0)+ "." + nam[1].get(0)).uppercase()
+}
+
+//Best Solution
+//=============
+
+fun abbrevName(name:String) = name.split(" ").map { it.first().toUpperCase() }.joinToString(".")
